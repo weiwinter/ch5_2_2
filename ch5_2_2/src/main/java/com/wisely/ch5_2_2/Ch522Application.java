edd3f8b90e4652584ebb7d8d1b1af956a6f38203
weiwinter <837144154@qq.com>
2019-04-11 18:05:53 +0800

    20190411-master-6.2.2-常规属性配置

--- ch5_2_2/src/main/java/com/wisely/ch5_2_2/Ch522Application.java
+++ ch5_2_2/src/main/java/com/wisely/ch5_2_2/Ch522Application.java
@@ -1,18 +1,29 @@
 package com.wisely.ch5_2_2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//使用XML配置
+//@ImportResource({"classpath:**1.xml","classpath:***2.xml",})
 @RestController
 @SpringBootApplication
 public class Ch522Application {
 
+	@Value("${book.author}")
+	private String author;
+	
+	@Value("${book.name}")
+	private String name;
+	
 	@RequestMapping("/")
 	String index() {
-		return "Hello! Spring Boot!";
+		return "Hello! Spring Boot!"+" book's name is "+name+";book's author is "+author;
 	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Ch522Application.class, args);
 	}
